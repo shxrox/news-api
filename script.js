ef6c55277dd51673d097c1d95866737afff61647
Sharon Devasudan <126344929+shxrox@users.noreply.github.com>
2024-07-12 15:00:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ searchButton.addEventListener("click", async ()=>{
 
 async function fetchNewsQuery(query){
     try {
-        const apiUrl = `https://newsapi.org/v2/everything?q=${query}&pageSize=10&apiKey=${apikey}`;
+        const apiUrl = `api URL`;
         const response = await fetch(apiUrl);
         const data = await response.json();
         return data.articles;
